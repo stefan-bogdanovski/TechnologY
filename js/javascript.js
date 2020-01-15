3daaff19267c26b8ceba7a5451c6a962b99bc111
stefan-bogdanovski <52039653+stefan-bogdanovski@users.noreply.github.com>
2020-01-15 23:39:10 +0100

    commit

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+$('#myCarousel').on('slide.bs.carousel', function () {
+    // do something...
+  })
+$(document).ready(function() {
+    $('#modal1').modal();
+});
+$(document).ready(function() {
+  $('#modal2').modal();
+});
+$(document).ready(function() {
+  $('#modal3').modal();
+});
+$(document).ready(function() {
+  $('#modal4').modal();
+});
+$(document).ready(function() {
+  $('#modal5').modal();
+});
+$(document).ready(function() {
+  $('#modal6').modal();
+});
